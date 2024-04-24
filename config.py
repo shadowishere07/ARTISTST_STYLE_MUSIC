9c9f1b80a8dd1864edae3f99148bda5ac2298217
ARTIST <154514127+wwwlbs22@users.noreply.github.com>
2024-04-24 12:08:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/LINKS_CHANNELL")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Quiz2xf")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dear_jindgi")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/lifelinenjoy")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
